79bff06fcfad9169460847f7b1e4c931cd0c89de
Samettkaya <kaya67380@gmail.com>
2021-06-19 01:59:12 +0300

    First

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Button, Image } from 'semantic-ui-react'
+
+export default function Dashboard() {
+    return (
+        <div>
+             <Button
+      color='red'
+      content='Like'
+      icon='heart'
+      label={{ basic: true, color: 'red', pointing: 'left', content: '2,048' }}
+    />
+    <Button
+      basic
+      color='blue'
+      content='Fork'
+      icon='fork'
+      label={{
+        as: 'a',
+        basic: true,
+        color: 'blue',
+        pointing: 'left',
+        content: '2,048',
+      }}
+    />
+        </div>
+    )
+}
